fcb717a51b19a69f97693172731ae2700e3b33e7
Nibin <nibin98prasad@gmail.com>
2022-06-13 23:04:02 +0530

    make it bette4r

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,7 +1,7 @@
 
 var db=require('../config/connection')
 var collection=require('../config/collections')
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 const { response } = require('express');
 const { ObjectID } = require('bson');
 var objectId = require("mongodb").ObjectID;
